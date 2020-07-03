3534b78231a9426dceb34a9516b789bfd10a6a8d
YeoYeJi <dpwl4321@naver.com>
2020-07-03 14:27:03 +0900

    HelloApp완료

--- StudyCS/HelloApp/Program.cs
+++ StudyCS/HelloApp/Program.cs
@@ -1,15 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;                                                           //namespace : 같은일들을 하기위한 클래스들의 집합체, System을 쓰겠다고 선언
+using static System.Console;                                            //Console = 클래스, 클래스를 정적으로 항상 쓰겠다고 선언
 
 namespace HelloApp
 {
     class Program
     {
+        // 프로그램 실행이 시작되는 곳
         static void Main(string[] args)
         {
+            if (args.Length == 0)
+            {
+                //Console.WriteLine("ex : HelloApp.exe <이름>");
+                WriteLine("ex : HelloApp.exe <이름>");
+                return;
+            }
+
+            //Console.WriteLine($"Hello, {args[0]}!");
+            WriteLine($"Hello, {args[0]}!");                            //using static System.Console 선언하면 Console. 안써도됨.
+            
         }
     }
 }
